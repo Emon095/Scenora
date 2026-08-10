aaa4442d134eef99209935aae6a1511844422c90
kathos <smshahrieremon@gmail.com>
2026-08-10 17:00:58 +0600

    Deploy Next.js application on GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }, { protocol: "https", hostname: "i.pravatar.cc" }] },
+  output: "export",
+  trailingSlash: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/Scenora" : "",
+  env: { NEXT_PUBLIC_BASE_PATH: process.env.GITHUB_ACTIONS ? "/Scenora" : "" },
+  images: {
+    unoptimized: true,
+    remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }, { protocol: "https", hostname: "i.pravatar.cc" }],
+  },
 };
 
 export default nextConfig;
